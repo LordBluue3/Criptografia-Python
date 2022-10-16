1af9b421edb47dd8f0bd288c36fc11921d5c2f9d
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-10-16 12:19:08 -0300

    Main class that launches the window and calls encryption methods

---
+++ main.py
@@ -0,0 +1,40 @@
+import PySimpleGUI as sg
+from message import Code
+import pyperclip as pc
+
+# Layout
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Texto para codificar')],
+    [sg.Multiline(size=(70, 10), key='codigo'), sg.Button('Codificar')],
+    [sg.Text('Texto para decodificar')],
+    [sg.Multiline(size=(70, 10), key='decodificar'), sg.Button('Decodificar'), sg.Button('Copiar')],
+    [sg.Text('Criado por Mikael Oliveira Candido Cerqueira ')]
+]
+
+# Window
+window = sg.Window('Tela de criptografia', layout)
+
+# Reading events
+global message_descrypt
+global message_encrypt
+while True:
+    events, value = window.read()
+    if events == sg.WINDOW_CLOSED:
+        break
+
+    if events == 'Codificar':
+        message = Code(value['codigo'], value['decodificar'])
+        message_encrypt = message.encrypt()
+        window.Element('decodificar').update(value=message_encrypt)
+        window.Element('codigo').update(value='')
+        print(value['decodificar'])
+
+    if events == 'Decodificar':
+        cryptography = Code(value['codigo'], value['decodificar'])
+        message_descrypt = cryptography.decrypt()
+        window.Element('codigo').update(value=message_descrypt)
+        window.Element('decodificar').update(value='')
+
+    if events == 'Copiar':
+        pc.copy(message_encrypt)
