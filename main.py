62203bdc7d4d20b9bcbc22e92ff1dbbd3c9b55ef
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-10-22 17:32:23 -0300

    Modificando a interface e printando mensagem de aviso no console

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ layout = [
     [sg.Text('Texto para codificar')],
     [sg.Multiline(size=(70, 10), key='codigo'), sg.Button('Codificar')],
     [sg.Text('Texto para decodificar')],
-    [sg.Multiline(size=(70, 10), key='decodificar'), sg.Button('Decodificar'), sg.Button('Copiar')],
-    [sg.Text('Criado por Mikael Oliveira Candido Cerqueira ')]
+    [sg.Multiline(size=(70, 10), key='decodificar'), sg.Button('Decodificar'), sg.Button('Copiar')]
 ]
 
 # Window
@@ -28,13 +27,14 @@ while True:
         message_encrypt = message.encrypt()
         window.Element('decodificar').update(value=message_encrypt)
         window.Element('codigo').update(value='')
-        print(value['decodificar'])
+        print("Mensagem criptografada", message_encrypt)
 
     if events == 'Decodificar':
         cryptography = Code(value['codigo'], value['decodificar'])
         message_descrypt = cryptography.decrypt()
         window.Element('codigo').update(value=message_descrypt)
         window.Element('decodificar').update(value='')
+        print(message_descrypt)
 
     if events == 'Copiar':
-        pc.copy(message_encrypt)
+        pc.copy("Mensagem Descriptografada", message_encrypt)
